7d365d1985533f718c2477cf3733207c204717e3
StarEX2 <131281386+StarEX2@users.noreply.github.com>
2023-11-07 22:02:28 -0500

    Update sum_of_multiples.py
    Excercism 'Sum of multiples'

--- exercises/practice/sum-of-multiples/sum_of_multiples.py
+++ exercises/practice/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,15 @@
-def sum_of_multiples(limit, multiples):
-    pass
+def sum_of_multiples(limit:int , multiples:list):
+    list_multiples=[]
+    for i in multiples:
+        for x in range(limit):
+            if x % i ==0 and x not in list_multiples:
+                list_multiples.append(x)
+    return sum(list_multiples)
+
+if __name__ == '__main__':
+    nums = input().split()
+    limit =int(nums[0])
+    multiples = [int(i) for i in nums[1:]]
+    result = sum_of_multiples(limit, multiples)
+    print(result)
+    
